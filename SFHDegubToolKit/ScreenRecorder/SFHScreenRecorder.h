3b0985631db1ca06c55893a354bf869d534cbf93
Econa77 <s.f.1992.ip@gmail.com>
2015-06-03 14:37:02 +0900

    Add library

---
+++ SFHDegubToolKit/ScreenRecorder/SFHScreenRecorder.h
@@ -0,0 +1,13 @@
+//
+//  SFHScreenRecorder.h
+//  DebugToolKit
+//
+//  Created by 古林　俊祐　 on 2015/06/03.
+//  Copyright (c) 2015年 古林　俊祐　. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SFHScreenRecorder : NSObject
+
+@end
---
+++ SFHDegubToolKit/ScreenRecorder/SFHScreenRecorder.m
@@ -0,0 +1,13 @@
+//
+//  SFHScreenRecorder.m
+//  DebugToolKit
+//
+//  Created by 古林　俊祐　 on 2015/06/03.
+//  Copyright (c) 2015年 古林　俊祐　. All rights reserved.
+//
+
+#import "SFHScreenRecorder.h"
+
+@implementation SFHScreenRecorder
+
+@end
